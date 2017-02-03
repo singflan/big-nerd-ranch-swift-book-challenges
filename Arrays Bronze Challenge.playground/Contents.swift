c6baf0aee013be58b71366e8471c2cea50ede63a
Dustin Flanary <singflan@gmail.com>
2017-02-03 11:25:50 -0700

    Exercises from a few more chapters

---
+++ Arrays Bronze Challenge.playground/Contents.swift
@@ -0,0 +1,6 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var toDoList = ["Take out garbage", "Pay bills", "Cross off finished items"]
+toDoList.isEmpty
---
+++ Optionals Bronze Challenge.playground/Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Number of kids a parent has could be represeneted as Int as 0 is fine and appropriate 
+
+// The name of a person's pet is definitely an optional of String? There'd not likely be named " " 
